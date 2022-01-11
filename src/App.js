6dfefd05972ac15eee5e3996c45faaee2f24de8a
EDev <eduardolmedi00@gmail.com>
2022-01-11 16:59:16 -0600

    done pack

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <h1>Welcome to React Easy Pack!</h1>
+    <i>Done by <a target="noopener" href='https://github.com/EduardoDevOlmedo'>Eduardo Olmedo</a></i>
+    <h3>React DOCS: <a target="noopener" href='https://reactjs.org/docs/getting-started.html#:~:text=The%20React%20documentation%20assumes%20some%20familiarity%20with%20programming,this%20JavaScript%20overview%20to%20check%20your%20knowledge%20level.'>Docs</a></h3>
+    </React.Fragment>
   );
 }
 
